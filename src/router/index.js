ad4e0209dcf8bcc4567b596e71ccdc75355e1a0d
SarapMax <teerpong.me@gmail.com>
2020-04-01 20:17:12 +0700

    Added axios.

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
+Vue.use(VueAxios, axios)
 
 export default new Router({
   mode: 'history',
